b603a208e574db558899351d21f12b92a4225adf
CardManj <c.manjarres2003@gmail.com>
2024-08-26 11:43:30 -0500

    style: :art: new transitions

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,15 +10,7 @@ export function Button() {
   const [quantity, setQuantity] = useState(1);
 
   const handleToggle = () => {
-    if (isActive) {
-      alert("You're shure?");
-      // Si el botón está activo, lo pasamos a inactivo y reseteamos la cantidad
-      setIsActive(false);
-      setQuantity(1);
-    } else {
-      // Si el botón está inactivo, lo activamos
-      setIsActive(true);
-    }
+    setIsActive((prevIsActive) => !prevIsActive);
   };
 
   const handleIncrement = () => {
@@ -30,13 +22,13 @@ export function Button() {
       setQuantity((prevQuantity) => prevQuantity - 1);
     }
   };
-
+  // absolute inset-x-0 top-1/2 transform -translate-y-1/2
   return (
-    <div className="absolute items-center transition-colors delay-75">
+    <div className=" items-center transition-colors delay-75">
       {!isActive ? (
         // Botón inactivo
         <button
-          className="flex items-center justify-center border rounded-3xl w-30 h-fit px-3 py-2 border-MentorRed bg-white text-xs"
+          className="flex items-center justify-center border rounded-3xl w-30 h-fit px-3 py-2 border-MentorRed bg-white text-xs font-semibold"
           onClick={handleToggle}
         >
           <AddToCardIcon className="mr-2" />
@@ -51,9 +43,7 @@ export function Button() {
           >
             <DecrementIcon className="" />
           </button>
-          <span className="px-5" onClick={handleToggle}>
-            {quantity}
-          </span>
+          <span className="px-5">{quantity}</span>
           <button
             className="px-1 flex items-center justify-center  border border-white py-1 hover:bg-white hover:text-MentorRed rounded-full"
             onClick={handleIncrement}
--- src/components/gallery/Card.tsx
+++ src/components/gallery/Card.tsx
@@ -31,22 +31,22 @@ const getImageByDevice = (image: ImageType): string => {
 
 export default function Card() {
   return (
-    <section className="grid grid-cols-3 gap-10 w-fit mr-5 ">
+    <section className=" grid grid-cols-3 gap-10 w-fit mr-5 ">
       {data_group.map((data: Data) => (
         <section
           key={data.id}
           className="w-full rounded-t-md rounded-b-xl hover:shadow-xl"
         >
-          <section className="flex flex-col justify-center align-center items-center">
+          <section className="overflow-hidden flex flex-col justify-center align-center items-center">
             {/* Llama a la función para obtener la imagen según el dispositivo */}
             <img
               src={getImageByDevice(data.image)}
               alt={`${data.name} image`}
-              className="w-full rounded-xl"
+              className="w-full rounded-xl object-cover transition-transform duration-500 ease-in-out transform hover:scale-110"
             />
             <Button />
           </section>
-          <section className="px-1 rounded-b-xl flex flex-col py-2 mt-3 ">
+          <section className="px-2 rounded-b-xl flex flex-col py-2 mt-3 ">
             <span className="text-xs text-gray-400 font-light mb-1">
               {data.category}
             </span>
